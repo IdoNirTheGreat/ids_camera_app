24944f7264f3358458a63e3c31cd3173b1b09748
IdoNirTheGreat <Idonir777@gmail.com>
2025-01-14 15:34:38 +0200

    Created some useful functions to make things easier.

--- main.c
+++ main.c
@@ -1,43 +1,77 @@
 #include <stdio.h>
 #include "ueye.h"
 
-int main() {
-    // Setup the iDS camera:
-    HIDS hCam = 0;
-
-    // Try connecting to the camera:
-    INT nRet = is_InitCamera (&hCam, NULL);
+void connectToCamera(HIDS* phCam)
+{
+    // Connect to the camera:
+    INT nRet = is_InitCamera(phCam, NULL);
     
     if (nRet != IS_SUCCESS)
     {
         printf("Error initializing camera.\n");
-        return -1;
+        exit(-1);
     }
     
-    printf("Connected successfully to camera %d\n", hCam);
+    printf("Connected successfully to camera %d\n", *phCam);
+}
 
+void setExternalTrigger(HIDS hCam)
+{
+    // Set trigger to external mode:
+    INT nTriggerMode = IS_SET_TRIGGER_LO_HI;
+    INT nRet = is_SetExternalTrigger(hCam, nTriggerMode);
+
+    if (nRet != IS_SUCCESS)
+    {
+        printf("Error setting external post-trigger.\n");
+        exit(-3);
+    }
+
+    printf("Successfully set external post-trigger for camera %d.\n", hCam);
+}
+
+void setGlobalShutter(HIDS hCam)
+{
     // Set the camera to be in global shutter mode:
     INT nShutterMode = IS_DEVICE_FEATURE_CAP_SHUTTER_MODE_GLOBAL;
-    nRet = is_DeviceFeature(hCam, IS_DEVICE_FEATURE_CMD_GET_SHUTTER_MODE, (void*)&nShutterMode, sizeof(nShutterMode));
+    INT nRet = is_DeviceFeature(hCam, IS_DEVICE_FEATURE_CMD_GET_SHUTTER_MODE, (void*)&nShutterMode, sizeof(nShutterMode));
 
     if (nRet != IS_SUCCESS)
     {
         printf("Error setting global shutter.\n");
-        return -2;
+        exit(-2);
     }
 
     printf("Successfully set global shutter mode for camera %d.\n", hCam);
 
-    // Try disconnecting from the camera:
-    nRet = is_ExitCamera(hCam);
+}
+
+void disconnectFromCamera(HIDS hCam)
+{
+    // Disconnect from the camera:
+    INT nRet = is_ExitCamera(hCam);
 
     if (nRet != IS_SUCCESS)
     {
         printf("Error disconnecting from camera.\n");
-        return -2;
+        exit(-2);
     }
     
     printf("Disconnected successfully from camera %d.\n", hCam);
 
+}
+
+int main()
+{
+    HIDS hCam = 0;  // Initate the hCam (handle for the iDS camera)
+
+    connectToCamera(&hCam);
+
+    setExternalTrigger(hCam);
+
+    setGlobalShutter(hCam);
+
+    disconnectFromCamera(hCam);
+    
     return 0;
 }
